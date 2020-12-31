b8cbb65a3e10a613575cd684dcc15d9c2ef71b16
Ofey Chan <ofey206@gmail.com>
2020-12-31 21:36:09 +0800

    Reformat some helpers

--- tests/hand_run_tests/recevefile.py
+++ tests/hand_run_tests/recevefile.py
@@ -1,18 +1,14 @@
 import socket
 import sys
 s = socket.socket()
-s.bind(("localhost",9999))
-s.listen(10) # Acepta hasta 10 conexiones entrantes.
-sc, address = s.accept()
+s.connect(("localhost",9999))
 
-print(address)
 f = open("tmp",'wb') # Open in binary
 # Recibimos y escribimos en el fichero
-l = sc.recv(1024)
+l = s.recv(1024)
 while (l):
     f.write(l)
-    l = sc.recv(1024)
+    l = s.recv(1024)
 f.close()
 
-sc.close()
 s.close()
--- tests/hand_run_tests/sendfile.py
+++ tests/hand_run_tests/sendfile.py
@@ -2,10 +2,17 @@ import socket
 import sys
 
 s = socket.socket()
-s.connect(("localhost",9999))
+s.bind(("127.0.0.1", 11111))
+s.listen()
+sc, address = s.accept()
+
+print(address)
+
 f = open ("LICENSE", "rb")
 l = f.read(1024)
 while (l):
-    s.send(l)
+    sc.send(l)
     l = f.read(1024)
+
+sc.close()
 s.close()
